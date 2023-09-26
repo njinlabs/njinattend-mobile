4cd30a642fbf9708cb23c188dce0d962ab6ce02e
akbaraditamasp <akbaraditamasp@gmail.com>
2023-09-27 03:03:15 +0700

    add: setting

--- app/(root)/setting.tsx
+++ app/(root)/setting.tsx
@@ -1,3 +1,61 @@
+import { Text, View } from "react-native";
+import styles, { colors, fonts } from "../../src/styles";
+import { Ionicons } from "@expo/vector-icons";
+import MenuList from "../../src/components/MenuList";
+
 export default function Setting() {
-  return null;
+  return (
+    <View style={styles.container}>
+      <View
+        style={{
+          backgroundColor: colors.white,
+          padding: 24,
+          alignItems: "center",
+        }}
+      >
+        <View
+          style={{
+            backgroundColor: colors.grayscale[100],
+            width: 84,
+            height: 84,
+            borderRadius: 84 / 2,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Ionicons
+            name="md-person-outline"
+            size={32}
+            color={colors.grayscale[400]}
+          />
+        </View>
+        <Text
+          style={{
+            ...fonts.montserratBold,
+            fontSize: 16,
+            color: colors.grayscale[800],
+            marginTop: 16,
+          }}
+        >
+          Akbar Aditama S.P.
+        </Text>
+        <Text style={{ ...styles.baseText, marginTop: 4, marginBottom: 2 }}>
+          Software Engineer
+        </Text>
+        <Text style={styles.baseText}>836827674</Text>
+      </View>
+      <View
+        style={{
+          marginVertical: 12,
+          paddingLeft: 16,
+          paddingBottom: 24,
+          paddingTop: 8,
+          backgroundColor: colors.white,
+        }}
+      >
+        <MenuList icon="lock-closed-outline">Ganti Password</MenuList>
+        <MenuList icon="log-out-outline">Keluar</MenuList>
+      </View>
+    </View>
+  );
 }
---
+++ src/components/MenuList.tsx
@@ -0,0 +1,44 @@
+import { Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import styles, { colors } from "../styles";
+
+type MenuListProps = {
+  children: React.ReactNode;
+  icon: keyof typeof Ionicons.glyphMap;
+};
+
+export default function MenuList({ children, icon }: MenuListProps) {
+  return (
+    <View style={{ flexDirection: "row", alignItems: "center" }}>
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          width: 32,
+        }}
+      >
+        <Ionicons name={icon} size={20} color={colors.grayscale[800]} />
+      </View>
+      <View
+        style={{
+          flex: 1,
+          borderBottomWidth: 1,
+          paddingVertical: 16,
+          borderBottomColor: colors.grayscale[100],
+          marginLeft: 16,
+        }}
+      >
+        <Text
+          style={{
+            ...styles.baseText,
+            fontSize: 16,
+            marginTop: 4,
+            color: colors.grayscale[700],
+          }}
+        >
+          {children}
+        </Text>
+      </View>
+    </View>
+  );
+}
